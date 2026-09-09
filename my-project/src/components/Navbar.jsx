424c93c9f921010d6edb40c88ccffb07b9224490
Destiny <atawodidestiny@gmail.com>
2026-09-09 14:22:08 +0100

    navbar

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import {FaMoon, FaSun, FaDumbbell} from 'react-icons/fa'
 
 const Navbar = () => {
   return (
-    <div className='bg-red-400 py-10'>Fitness pro</div>
+    <header>
+      <div className="container flex items-center justify-between px-8 py-4">
+
+        <div className="flex items-center gap-2">
+          <FaDumbbell className="text-orange-400 text-2xl"/>
+          <h1 className="text-xl font-bold">Fitness Pro</h1>
+        </div>
+
+        <nav className="hidden md:flex gap-8 font-medium">
+          <a href="/" className=" hover:text-orange-400 transition-duration-300">Home</a>
+          <a href="/" className="">Features</a>
+          <a href="/" className="">Pricing</a>
+          <a href="/" className="">Testimonials</a>
+          <a href="/" className="">Testimonials</a>
+        </nav>
+
+        <button>
+          <FaSun className="text-yellow-400 text-lg" />
+        </button>
+
+      </div>
+    </header>
   )
 }
 
